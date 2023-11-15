18fdd008697f3390149d0bee8b80c066923573ad
englishwing <vietnam@englishwing.com>
2023-11-15 10:21:44 +0700

    test

--- src/utils/mongodb_utils.py
+++ src/utils/mongodb_utils.py
@@ -5,10 +5,8 @@ from pymongo import MongoClient
 def get_database(dbname: str) -> str:
     CONNECTION_STRING = "mongodb+srv://vinhprs:19052002@cluster0.znnks5j.mongodb.net/?retryWrites=true&w=majority"
 
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
 
-    # Create the database for our example (we will use the same database throughout the tutorial
     return client[dbname]
 
 
